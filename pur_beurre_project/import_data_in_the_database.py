fae2961063afcebea1e48e735c00ed6cbd0f3420
AlexBardDev <alexandre.bardiaux@outlook.com>
2018-05-07 15:59:55 +0200

    Database scripts moved

--- pur_beurre_project/import_data_in_the_database.py
+++ pur_beurre_project/import_data_in_the_database.py
@@ -10,17 +10,14 @@ import requests
 #Standard library
 
 #Local library
-from models import Food, NutritionalInformation, Category
+from food_substitute.models import Food, NutritionalInformation, Category
 
 #Some constants
 URL_ALL_CATEGORIES = "https://fr.openfoodfacts.org/categories.json"
 URL_CATEGORY = "https://fr.openfoodfacts.org/categorie/{}/{}.json"
 URL_FOOD = "https://fr.openfoodfacts.org/api/v0/produit/{}.json"
 
-def import_data():
-    """This function imports all the needed data in the database."""
-
-    categories = requests.get(URL_ALL_CATEGORIES).json()["tags"]
-    for i in range(100):
-        name = categories[i]["url"].split("/")[-1]
-        print(name)
+categories = requests.get(URL_ALL_CATEGORIES).json()["tags"]
+for i in range(100):
+    name = categories[i]["url"].split("/")[-1]
+    print(name)
