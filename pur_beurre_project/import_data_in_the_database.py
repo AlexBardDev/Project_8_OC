3e15f967d547232e0b1a61a82a996762824aa375
AlexBardDev <alexandre.bardiaux@outlook.com>
2018-05-07 16:12:33 +0200

    Categories saved in the database

--- pur_beurre_project/import_data_in_the_database.py
+++ pur_beurre_project/import_data_in_the_database.py
@@ -5,11 +5,11 @@ import django
 #Standard library
 import os
 
-#Local library
-from food_substitute.models import Food, NutritionalInformation, Category
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pur_beurre_project.settings')
 django.setup()
+
+#Local library
+from food_substitute.models import Food, NutritionalInformation, Category
 
 #Some constants
 URL_ALL_CATEGORIES = "https://fr.openfoodfacts.org/categories.json"
@@ -18,5 +18,6 @@ URL_FOOD = "https://fr.openfoodfacts.org/api/v0/produit/{}.json"
 
 categories = requests.get(URL_ALL_CATEGORIES).json()["tags"]
 for i in range(100):
-    name = categories[i]["url"].split("/")[-1]
-    print(name)
+    name_category = categories[i]["url"].split("/")[-1]
+    new_category = Category.objects.create(name=name_category)
+    print("""{} saved.""".format(name_category))
