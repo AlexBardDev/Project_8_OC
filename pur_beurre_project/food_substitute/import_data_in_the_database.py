4f002610391febbb694d08676c93a44d22009418
AlexBardDev <alexandre.bardiaux@outlook.com>
2018-05-07 11:40:22 +0200

    New library added

---
+++ pur_beurre_project/food_substitute/import_data_in_the_database.py
@@ -0,0 +1,7 @@
+#External library
+import requests
+
+#Some constants
+URL_CATEGORY = "https://fr.openfoodfacts.org/categorie/{}/{}.json"
+URL_FOOD = "https://fr.openfoodfacts.org/api/v0/produit/{}.json"
+
