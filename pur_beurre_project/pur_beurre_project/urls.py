d474d6ce87a101444f20e83d14af4fa78ab72424
AlexBardDev <alexandre.bardiaux@outlook.com>
2018-05-08 12:41:20 +0200

    URL route improved

--- pur_beurre_project/pur_beurre_project/urls.py
+++ pur_beurre_project/pur_beurre_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('food/', include('food_substitute.urls')),
+    path('', include('food_substitute.urls')),
 ]
