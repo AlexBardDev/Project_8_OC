58f503445d5e43e9b9455456551ca125899c6f82
AlexBardDev <alexandre.bardiaux@outlook.com>
2018-05-14 19:21:48 +0200

    Admin added

--- pur_beurre_project/food_substitute/admin.py
+++ pur_beurre_project/food_substitute/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+from django.contrib import admin 
+from .models import Food, Category, NutritionalInformation, Bookmark
 
-# Register your models here.
+admin.site.register(Food)
+admin.site.register(Category)
+admin.site.register(NutritionalInformation)
+admin.site.register(Bookmark)
