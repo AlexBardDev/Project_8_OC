ae1c7e1fdb5c7114120e1c2d240172589170f463
AlexBardDev <alexandre.bardiaux@outlook.com>
2018-05-15 10:17:39 +0200

    Admin interface done

--- pur_beurre_project/food_substitute/admin.py
+++ pur_beurre_project/food_substitute/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin 
 from .models import Food, Category, NutritionalInformation, Bookmark
 
-admin.site.register(Food)
-admin.site.register(Category)
+class FoodAdmin(admin.ModelAdmin):
+    """This class customizes the 'Food' data for the admin interface."""
+    
+    list_display = ('name', 'id_category', 'nutriscore',)
+    list_filter = ('nutriscore', 'id_category',)
+    search_fields = ('name',)
+
+admin.site.register(Food, FoodAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    """This class customizes the 'Category' data for the admin interface."""
+
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+admin.site.register(Category, CategoryAdmin)
+
 admin.site.register(NutritionalInformation)
 admin.site.register(Bookmark)
--- pur_beurre_project/food_substitute/models.py
+++ pur_beurre_project/food_substitute/models.py
@@ -7,6 +7,9 @@ class Category(models.Model):
 
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 class NutritionalInformation(models.Model):
     """This class is the 'NutritionalInformation' table in the database. It
     stores all the nutritional information for 100g."""
@@ -20,6 +23,9 @@ class NutritionalInformation(models.Model):
     salt = models.FloatField()
     sodium = models.FloatField()
 
+    def __str__(self):
+        return """Informations nutritionelles pour le produit : {}.""".format(self.food_set.all()[0].name)
+
 class Food(models.Model):
     """This class is the 'Food' table in the database. It stores all the
     information about food."""
@@ -31,10 +37,16 @@ class Food(models.Model):
     image = models.URLField()
     link = models.URLField()
 
+    def __str__(self):
+        return self.name
+
 class Bookmark(models.Model):
     """This class is the 'Bookmark' table in the database. It stores all the
     bookmarks of the users."""
 
     id_user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_product = models.ForeignKey(Food, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return """{} a enregistré {}.""".format(self.id_user.username, self.id_product.name)
 
