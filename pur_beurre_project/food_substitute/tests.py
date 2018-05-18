f1d065512e64296b954e7218d46ebfbe94038dc2
AlexBardDev <alexandre.bardiaux@outlook.com>
2018-05-18 11:57:04 +0200

    Last integration tests added

--- pur_beurre_project/food_substitute/tests.py
+++ pur_beurre_project/food_substitute/tests.py
@@ -1,10 +1,8 @@
-#Create a DATABASE for the tests !!!!
-
 #Import django libraries
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
-from .models import Food
+from .models import Food, Category, NutritionalInformation
 from selenium.webdriver.firefox.webdriver import WebDriver
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
@@ -17,9 +15,25 @@ class ViewsTests(TestCase):
         pass
 
     def new_food(self):
-        """This function creates a new row in the 'Food' table."""
+        """This function creates new rows in the database."""
 
-        Food.objects.create()
+        new_category = Category.objects.create(name="produits-a-tartiner")
+
+        nutri_product = NutritionalInformation.objects.create(calories=539,
+                fat=30, saturated_fat=10, carbohydrates=57, sugars=56, proteins=6,
+                salt=0, sodium=0)
+        product = Food.objects.create(name="Nutella", nutriscore="e",
+                image="https://static.openfoodfacts.org/images/products/301/762/042/1006/front_fr.87.400.jpg",
+                link="https://fr.openfoodfacts.org/produit/3017620421006/nutella-ferrero",
+                id_category=new_category, id_nutritional_information=nutri_product)
+
+        nutri_substitute = NutritionalInformation.objects.create(calories=539,
+                fat=31, saturated_fat=5, carbohydrates=57, sugars=55, proteins=5,
+                salt=0, sodium=0)
+        substitute = Food.objects.create(name="Pralina", nutriscore="d",
+                image="https://static.openfoodfacts.org/images/products/326/385/223/1719/front_fr.24.400.jpg",
+                link="https://fr.openfoodfacts.org/produit/3263852231719/pralina-leader-price",
+                id_category=new_category, id_nutritional_information=nutri_substitute)
 
     def create_user(self):
         """This function creates a new user for a testing purpose."""
@@ -46,12 +60,14 @@ class ViewsTests(TestCase):
     def test_search(self):
         """This function tests the 'search' view."""
 
+        self.new_food()
         response = self.client.get(reverse('search', args=['Nutella']))
         assert response.status_code == 200
 
     def test_display(self):
         """This function tests the 'display' view."""
-
+        
+        self.new_food()
         response = self.client.get(reverse('display', args=['Nutella']))
         assert response.status_code == 200
 
@@ -130,6 +146,7 @@ class ViewsTests(TestCase):
 
         self.create_user()
         self.login_user()
+        self.new_food()
         response = self.client.get(reverse('save_product', args=['Nutella']))
         assert response.status_code == 302
 
@@ -153,22 +170,49 @@ class IntegrationTests(StaticLiveServerTestCase):
         cls.selenium.quit()
         super().tearDownClass()
 
+    def import_data(self):
+        """This method imports some data in the database."""
+       
+        new_category = Category.objects.create(name="produits-a-tartiner")
+
+        nutri_product = NutritionalInformation.objects.create(calories=539,
+                fat=30, saturated_fat=10, carbohydrates=57, sugars=56, proteins=6,
+                salt=0, sodium=0)
+        product = Food.objects.create(name="Nutella", nutriscore="e",
+                image="https://static.openfoodfacts.org/images/products/301/762/042/1006/front_fr.87.400.jpg",
+                link="https://fr.openfoodfacts.org/produit/3017620421006/nutella-ferrero",
+                id_category=new_category, id_nutritional_information=nutri_product)
+
+        nutri_substitute = NutritionalInformation.objects.create(calories=539,
+                fat=31, saturated_fat=5, carbohydrates=57, sugars=55, proteins=5,
+                salt=0, sodium=0)
+        substitute = Food.objects.create(name="Pralina", nutriscore="d",
+                image="https://static.openfoodfacts.org/images/products/326/385/223/1719/front_fr.24.400.jpg",
+                link="https://fr.openfoodfacts.org/produit/3263852231719/pralina-leader-price",
+                id_category=new_category, id_nutritional_information=nutri_substitute)
+
+    def create_new_user(self):
+        """This method creates a new user in the database."""
+
+        User.objects.create_user("test@mail.com", "test@mail.com", "passwd")
+
     def test_get_home_page_do_a_research_and_see_a_product(self):
         """This test gets the home page. Then, submits a form and does a
         research. And finally, displays the product page."""
 
+        self.import_data()
         self.selenium.get(self.live_server_url)
-        search_input = self.selenium.find_element_by_name("research")
+        search_input = self.selenium.find_element_by_css_selector("#about input[type='search']")
         search_input.send_keys("Nutella")
         self.selenium.find_element_by_class_name("btn-outline-primary").click()
-        self.selenium.find_element_by_css_selector(".row a").click()
-        content = self.selenium.find_element_by_tag_name("a")
+        self.selenium.find_element_by_css_selector(".row a[href*='/product/']").click()
+        content = self.selenium.find_element_by_css_selector("a[target='_blank']")
         assert content.text == "Voir la fiche complète d'OpenFoodFacts"
     
     def test_create_new_account(self):
         """This test gets the 'create account' page and simulates a new user."""
 
-        self.selenium.get(reverse('create_account'))
+        self.selenium.get(self.live_server_url + "/create_account/")
         email_input = self.selenium.find_element_by_name("email")
         email_input.send_keys("test@mail.com")
         password_input = self.selenium.find_element_by_name("password")
@@ -182,17 +226,26 @@ class IntegrationTests(StaticLiveServerTestCase):
         does a research, saves a new substitute, sees the bookmarks page and
         logs out."""
 
-        self.selenium.get(reverse('login'))
+        self.import_data()
+        self.create_new_user()
+
+        self.selenium.get(self.live_server_url + "/login/")
         email_input = self.selenium.find_element_by_name("email")
         email_input.send_keys("test@mail.com")
         password_input = self.selenium.find_element_by_name("password")
         password_input.send_keys("passwd")
         self.selenium.find_element_by_css_selector("input[type='submit']").click()
 
-        search_input = self.selenium.find_element_by_name("research")
+        search_input = self.selenium.find_element_by_css_selector("#about input[type='search']")
         search_input.send_keys("Nutella")
         self.selenium.find_element_by_class_name("btn-outline-primary").click()
 
+        substitute_name = self.selenium.find_element_by_css_selector(".row a p").text
         self.selenium.find_element_by_css_selector(".row p a").click()
+        
+        self.selenium.find_element_by_css_selector("nav a[href*='/bookmark/']").click()
+        product_name = self.selenium.find_element_by_css_selector("a p").text
 
-#click on the carrot menu/logout
+        self.selenium.find_element_by_css_selector("nav a[href*='/logout/']").click()
+
+        assert substitute_name == product_name
